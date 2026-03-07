5f7854a1e3527f944023cafc32d4bb6c7560d574
Ashutosh Kumar <ashutosh0885333@gmail.com>
2026-03-07 17:47:57 +0530

    Add files via upload
    WAP TO WRITE THE TABLE OF THE NUMBER GIVEN BY THE USER

---
+++ table.c
@@ -0,0 +1,15 @@
+//WAP TO WRITE THE TABLE OF THE NUMBER GIVEN BY THE USER 
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number n:");
+    scanf("%d",&n);
+    for(int i=1;i<=10;i++){
+        printf("%d \n",n*i);
+
+    }
+    return 0;
+
+
+}
