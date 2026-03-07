0f5aafb915421812880b8144268d9d847e4f4ff5
Ashutosh Kumar <ashutosh0885333@gmail.com>
2026-03-07 16:43:24 +0530

    Add files via upload

---
+++ WAP RO THE NUMBER FROM 0 TO 'N' AND 'N' IS DEFINED BY USER.c
@@ -0,0 +1,15 @@
+//WAP RO THE NUMBER FROM 0 TO 'N' AND 'N' IS DEFINED BY USER
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number n \n");
+    scanf("%d",&n);
+    int i=0;
+    while(i<=n){
+        printf("%d \n",i);
+        i++;
+
+    }
+    return 0;
+}
