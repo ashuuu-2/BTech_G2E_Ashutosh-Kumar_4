943e290ca8486b3de7644a5877df52a6668e5c85
Ashutosh Kumar <ashutosh0885333@gmail.com>
2026-03-17 16:30:23 +0530

    Add files via upload
    //WAP TO MULTIPLY TWO MATRIX GIVEN BY THE USER

---
+++ multiplymatrix.c
@@ -0,0 +1,33 @@
+//WAP TO MULTIPLY TWO MATRIX GIVEN BY THE USER
+#include<stdio.h>
+int main(){
+    int a[3][3],b[3][3],c[3][3];
+    printf("enter the element of the first matrix");
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("enetr the second matrix element");
+
+    for(int i=0;i<3;i++){
+        for(int j=0;i<3;i++){
+            scanf("%d",&b[i][j]);
+        }
+    }
+    //MULTIPLICATION OF THE TWO MATRIX
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            c[i][j];
+            for(int k=0;k<3;k++){
+                c[i][j]+=a[i][j]*b[i][j];}
+        }
+        }
+        }
+        printf("tje result is");
+        for(int i=0;i<3;i++){
+
+            for(int j=0;i<3;i++)
+            
+        }
+}
